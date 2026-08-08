0d010dded4b785dfe652785de37e7386b9af7c42
Tsuyakashi <kashakdaniil@gmail.com>
2026-08-08 03:46:03 +0300

    Update Terraform backend configuration to use dynamic region and correct S3 endpoint. Add CI SSH public key variable to example Terraform variables for improved user guidance.

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket                      = "iac-proxmox-lab-tfstate"
     key                         = "terraform.tfstate"
-    region                      = "us-east-1"
-    endpoints                   = { s3 = "http://192.168.100.15:9000" }
+    region                      = "auto"
+    endpoints                   = { s3 = "http://192.168.100.10:9000" }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
