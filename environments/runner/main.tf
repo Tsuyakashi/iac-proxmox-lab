32c838086c7d72ad7283eeef261b48365051e182
Tsuyakashi <kashakdaniil@gmail.com>
2026-09-08 03:58:01 +0300

    chore: bump Vault to 2.1.0
    - vault-lxc-init.sh: VAULT_VERSION 1.19.0 -> 2.1.0, CT memory 512 -> 1024 MB
    - runner: pull vault_2.1.0 binary from the internal mirror

--- environments/runner/main.tf
+++ environments/runner/main.tf
@@ -54,7 +54,7 @@ module "ci_runner" {
     "systemctl enable --now docker",
     "curl -o /usr/local/bin/terraform http://192.168.100.100:9000/tools/terraform_1.15.8_linux_amd64",
     "chmod +x /usr/local/bin/terraform",
-    "curl -o /usr/local/bin/vault http://192.168.100.100:9000/tools/vault_1.19.0_linux_amd64",
+    "curl -o /usr/local/bin/vault http://192.168.100.100:9000/tools/vault_2.1.0_linux_amd64",
     "chmod +x /usr/local/bin/vault",
     "curl -o /usr/local/bin/mc https://dl.min.io/aistor/mc/release/linux-amd64/mc",
     "chmod +x /usr/local/bin/mc",
